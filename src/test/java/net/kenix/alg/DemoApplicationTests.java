2d7950e9080cc2480d4f7dac168785df9455042b
kenix <zhongda.zhao@gmail.com>
2020-05-31 12:30:17 +0200

    init commit

---
+++ src/test/java/net/kenix/alg/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.kenix.alg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
